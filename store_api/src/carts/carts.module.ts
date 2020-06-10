bf3e3363708e070a116cdff068ad8273bc85bf31
Robert Myers <rtmyers.com@gmail.com>
2020-06-10 11:26:19 -0600

    Create carts.module.ts

---
+++ store_api/src/carts/carts.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { CartsService } from './carts.service';
+import { CartsController } from './carts.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { CartsSchema } from './schemas/carts.schema';
+
+@Module({
+  imports: [MongooseModule.forFeature([{ name: 'Carts', schema: CartsSchema }])],
+  providers: [CartsService],
+  controllers: [CartsController]
+})
+export class CartsModule {}
