50d69c20dabef6616364fe8cad2ccb4b351eaff9
Robert Myers <rtmyers.com@gmail.com>
2020-06-11 17:47:53 -0600

    Update app.e2e-spec.ts

--- store_api/src/app/__tests__/app.e2e-spec.ts
+++ store_api/src/app/__tests__/app.e2e-spec.ts
@@ -1,24 +1,26 @@
 import * as request from 'supertest';
-import { Test } from '@nestjs/testing';
+import { Test, TestingModule } from '@nestjs/testing';
+import { AppController } from '../app.controller';
 import { ApplicationModule } from '../app.module';
 import { INestApplication } from '@nestjs/common'; 
 
-describe('AppController (e2e)', () => {
+describe('AppController', () => {
     let app: INestApplication;
-   
-    beforeAll(async () => {
-     const moduleFixture = await Test.createTestingModule({
-      imports: [ApplicationModule],
-     }).compile();
-   
-     app = moduleFixture.createNestApplication();
-     await app.init();
+    let appController: AppController;
+  
+    beforeEach(async () => {
+      const module: TestingModule = await Test.createTestingModule({
+        controllers: [AppController],
+      }).compile();
+  
+      appController = module.get<AppController>(AppController);
+      app = module.createNestApplication();
+        await app.init();
     });
-   
     it('/ (GET)', () => {
         return request(app.getHttpServer())
         .get('/')
         .expect(200)
         .expect('Hello World!');
     });
-   });
+   });  
